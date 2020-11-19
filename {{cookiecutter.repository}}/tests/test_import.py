408728ab6c1764b415b1b2676b9c4c0c86a067c4
Shiny Brar <charanjotbrar@gmail.com>
2020-11-19 12:46:16 -0500

    Fixed tests.

--- {{cookiecutter.repository}}/tests/test_import.py
+++ {{cookiecutter.repository}}/tests/test_import.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 """Generic tests for frb-datatrails."""
-import datatrail
+import {{project}}
 
 
 def test_project_import():
     """Simple check to test for package importability."""
-    assert isinstance(datatrail.__file__, str)
+    assert isinstance({{project}}.__file__, str)
 
 
 def test_analysis_function():
     """Check if the seed function works."""
     flavor = "str"
-    uuid = datatrail.analysis.seed.get_uuid(flavor=flavor)
+    uuid = {{project}}.analysis.seed.get_uuid(flavor=flavor)
     assert isinstance(uuid, str)
