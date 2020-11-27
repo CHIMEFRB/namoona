c514a3c0a12496b4c20094692cb3313ca765a9bb
Shiny Brar <charanjotbrar@gmail.com>
2020-11-26 23:53:18 -0500

    Fixed issue with project template. -SB

--- {{cookiecutter.repository}}/tests/test_import.py
+++ {{cookiecutter.repository}}/tests/test_import.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 """Generic tests for frb-datatrails."""
-import {{project}}
+import {{cookiecutter.project}}
 
 
 def test_project_import():
     """Simple check to test for package importability."""
-    assert isinstance({{project}}.__file__, str)
+    assert isinstance({{cookiecutter.project}}.__file__, str)
 
 
 def test_analysis_function():
     """Check if the seed function works."""
     flavor = "str"
-    uuid = {{project}}.analysis.seed.get_uuid(flavor=flavor)
+    uuid = {{cookiecutter.project}}.analysis.seed.get_uuid(flavor=flavor)
     assert isinstance(uuid, str)
